2b853a345f03109eb488011316499a5ce9efd558
malcolm twhoo <morkeltry@mailfence.com>
2018-04-23 15:32:04 +0100

    added sections for section 1,2,3,5,6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +8,31 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="section1">
+          There is no other platform by eck as like <br />
+          There is no other platform by eck as like  <br />
+          There is no other platform by eck as like <br />
+          There is no other platform by eck as like  <br />
+          There is no other platform by eck as like <br />
+          There is no other platform by eck as like  <br />
+          There is no other platform by eck as like <br />
+          There is no other platform by eck as like  <br />
+          There is no other platform by eck as like <br />
+          There is no other platform by eck as like  <br />
+          There is no other platform by eck as like <br />
+          There is no other platform by eck as like  <br />
+        </section>
+        <section className="bg-pink-triangle">
+        </section>
+        <section className="section3">
+        </section>
+        <section className="section5">
+        </section>
+        <section className="section6">
+        </section>
+
+
+
       </div>
     );
   }
