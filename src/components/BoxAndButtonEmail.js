4e1cff8bb42deca840842de1b2c9aed11cd6fdde
malcolm twhoo <morkeltry@mailfence.com>
2018-04-24 15:47:17 +0100

    outline section 8 (footer finished) - displays text as address, 2x navbars, box-and-button email submit and social widgets

--- src/components/BoxAndButtonEmail.js
+++ src/components/BoxAndButtonEmail.js
@@ -1,12 +1,24 @@
 import React, { Component, } from 'react';
+import SocialWidgets from './SocialWidgets.js';
+import './styles/box-and-button-form.css';
+
+import buttonIcon from '../images/section7-buttonIcon.png';
+import browserImg from '../images/section6-browser.png';
 
 class BoxAndButtonEmail extends Component {
   render() {
-    return  (
-      <div>
-      </div>
+    return  (<form className="box-and-button-form">
+            <input type="email" className="text-input-narrow" placeholder="EMAIL" />
+          <input type="image" src={buttonIcon} className="submit-image-paper-plane" alt="Submit Form" />
+          <SocialWidgets />
+        </form>
     )
   }
 }
 
 export default BoxAndButtonEmail;
+
+
+
+        // <input type="submit" value="submit"
+        //     style={{background: buttonIcon, backgroundColor: '#FF6D6D'}} />
---
+++ src/data/social.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo1 from '../images/section7-logo1.png';
+import logo2 from '../images/section7-logo2.png';
+import logo3 from '../images/section7-logo3.png';
+import logo4 from '../images/section7-logo4.png';
+
+export default [
+  { image : <img src={logo1} alt="some ball thing" className="logo" /> },
+  { image : <img src={logo2} alt="fb" className="logo" /> },
+  { image : <img src={logo3} alt="g+" className="logo" /> },
+  { image : <img src={logo4} alt="tweet" className="logo" /> }
+]
